6ebe6e9c3fa65c013c00841403a78c1a5cd607f2
Gabriel Araujo <gabriel.houth@gmail.com>
2021-08-17 17:56:36 -0300

    feat: initial commit

---
+++ src/global/theme/styles.ts
@@ -0,0 +1,22 @@
+export const colors = {
+  background: '#FFFFFF',
+
+  titleColor: '#3F3F3F',
+  subtitleColor: '#828282',
+
+  border: '#AEAEAE',
+
+  primaryDark: '#6666FF',
+  primaryLight: '#E6E6FF',
+  primaryBlank: '#FFFFFF',
+
+  warning: '#FF5858',
+
+  available: '#219653',
+};
+
+export const fonts = {
+  titleFont100: 'Inter-Bold',
+  titleFont50: 'Inter-Medium',
+  subtitleFont: 'Inter-Regular',
+};
